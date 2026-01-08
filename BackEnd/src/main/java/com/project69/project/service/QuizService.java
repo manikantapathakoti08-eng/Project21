1250360157321710be88854e57617d1cc04ac1a8
manikantapathakoti08-eng <manikantapathakoti08@gmail.com>
2026-01-08 11:35:33 +0530

    Update QuizService.java

--- BackEnd/src/main/java/com/project69/project/service/QuizService.java
+++ BackEnd/src/main/java/com/project69/project/service/QuizService.java
@@ -1,16 +1,9 @@
 package com.project69.project.service;
-
 import com.project69.project.model.QuestionWrapper;
 import com.project69.project.model.QuizResult;
 import com.project69.project.model.Response;
 import org.springframework.http.ResponseEntity;
-
 import java.util.List;
-
-/**
- * Service interface for quiz operations.
- * (Unchanged from your provided version but included here for completeness.)
- */
 public interface QuizService {
     ResponseEntity<String> createQuiz(int noQ, String title);
     ResponseEntity<List<QuestionWrapper>> getQuizQuestions(Integer id);
